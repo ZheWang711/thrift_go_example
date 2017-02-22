ffcc40068281a3dde30b63ef0e1b2d302ed7bf4e
zhewang711 <zhw176@ucsd.edu>
2017-02-22 02:35:00 -0800

    build and run

---
+++ gen-go/include/server.go
@@ -0,0 +1,35 @@
+package include
+
+import (
+	"crypto/tls"
+	"fmt"
+	"git.apache.org/thrift.git/lib/go/thrift"
+	"github.com/ZheWang711/thrift_go_example/gen-go/tutorial"
+)
+
+func RunServer(transportFactory thrift.TTransportFactory, protocolFactory thrift.TProtocolFactory, addr string, secure bool) error {
+	var transport thrift.TServerTransport
+	var err error
+	if secure {
+		cfg := new(tls.Config)
+		if cert, err := tls.LoadX509KeyPair("server.crt", "server.key"); err == nil {
+			cfg.Certificates = append(cfg.Certificates, cert)
+		} else {
+			return err
+		}
+		transport, err = thrift.NewTSSLServerSocket(addr, cfg)
+	} else {
+		transport, err = thrift.NewTServerSocket(addr)
+	}
+
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%T\n", transport)
+	handler := NewCalculatorHandler()
+	processor := tutorial.NewCalculatorProcessor(handler)
+	server := thrift.NewTSimpleServer4(processor, transport, transportFactory, protocolFactory)
+
+	fmt.Println("Starting the simple server... on ", addr)
+	return server.Serve()
+}
